5bd34f1d49bafc9063d8abb2a1247361a36b4560
Joshua Manncleargit config --global user.email mann.photography@gmail.com <mann.photography@gmail.com>
2023-05-07 14:06:30 -0700

    Add responsive buy ticket section

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,39 +1,111 @@
-  
-  let bandsiteShows = [ 
+  const showsContainer = document.querySelector('.show');
+   
+  const bandShows = [ 
     { 
     Date: "Mon Sept 06 2021",
-    Venu: "Ronald Lane",
+    Venue: "Ronald Lane",
     Location: "San Francisco, CA",    
     },
 
     {
     Date: "Tue Sept 21 2021",
-    Venu: "Pier 3 East",
+    Venue: "Pier 3 East",
     Location: "San Francisco, CA"
     },
 
     {
       Date: "Fri Oct 15 2021",
-      Venu: "View Lounge", 
+      Venue: "View Lounge", 
       Location: "San Francisco, CA",
     },
 
     {
       Date: "Sat Nov 06 2021", 
-      Venu: "Hyatt Agency",
+      Venue: "Hyatt Agency",
       Location: "San Francisco, CA", 
     },
 
     {
       Date: "Fri Nov 26 2021",
-      Venu: "Moscow Center", 
+      Venue: "Moscow Center", 
       Location: "San Francisco, CA" 
     },
 
     {
       Date: "Wed Dec 15 2021",
-      Venu: "Press Club", 
+      Venue: "Press Club", 
       Location: "San Francisco, CA"
     },
   ];
+
+function displayShows(show) { 
+
+  for (let i = 0; i < bandShows.length; i++) {
+
+    const show = bandShows[i];
+
+
+  const showContainer = document.createElement('div');
+  showContainer.classList.add('show__container');
+  showsContainer.appendChild(showContainer);
+
+  // Date container
+  const showDate = document.createElement('div');
+  showDate.classList.add('show__date');  
+  showContainer.appendChild(showDate);
+
+  const dateHead = document.createElement('p');
+  dateHead.classList.add('show__container-silver')
+  dateHead.innerText = "Date";
+  showDate.appendChild(dateHead);
+
+  const date = document.createElement('p');
+  date.classList.add('show__date-num');
+  date.textContent = show.Date;
+  showDate.appendChild(date);
+  
+  // // Venue container
+  const showVenue = document.createElement('div');
+  showVenue.classList.add('show__venue');
+  showContainer.appendChild(showVenue);
+
+  const venueHead = document.createElement('p');
+  venueHead.classList.add('show__venue-silver');
+  venueHead.innerText = "Venue";
+  showVenue.appendChild(venueHead);
+
+  const venuePlace = document.createElement('p');
+  venuePlace.classList.add('show__venue-head');
+  venuePlace.textContent = show.Venue;
+  showVenue.appendChild(venuePlace);
+
+
+  // // Location container
+  const showLocal = document.createElement('div');
+  showLocal.classList.add('show__location');
+  showContainer.appendChild(showLocal);
+
+  const localHead = document.createElement('p');
+  localHead.classList.add('show__location-silver');
+  localHead.innerText = "Location";
+  showLocal.appendChild(localHead);
+ 
+  
+
+  const localPlace = document.createElement('p');
+  localPlace.classList.add('show__location-place');
+  localPlace.textContent = show.Location;
+  showLocal.appendChild(localPlace);  
+
+  const button = document.createElement('button');
+  button.classList.add('show__button');
+  button.innerText = "Buy Tickets";
+  showContainer.appendChild(button);
+
+
+  }
+}
+
+
+displayShows();
 
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,30 +1,24 @@
+// Form Event Handler 
 const form = document.querySelector('.form');
-// console.log(form);
-// const commentContainer = document.querySelector('.comments__container');
-
-
-
+const commentContainer = document.querySelector(".comments");
 
 // Pre populated Comment Cards.
 
 const commentCards = [
 
 { 
-image: "1",   
 name: "Connor Walton",
 date: "02/17/2021",
 comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
 },
 
 { 
-    image: "2",    
     name: "Emilie Beach",
     date: "01/09/2021",
     comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
 },
 
 { 
-    image: "3",
     name: "Miles Acosta",
     date: "12/20/2020",
     comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
@@ -32,13 +26,9 @@ comment: "This is art. This is inexplicable magic expressed in the purest way, e
 
 ];
 
+function displayComment() {
 
-const commentContainer = document.querySelector(".comments");
-// console.log("comment container", commentContainer);
-
-
-
-    commentCards.forEach(function(comments) {
+    commentCards.forEach((comments) => {
 
         const container = document.createElement("div");
         container.classList.add("comments__container");
@@ -52,31 +42,56 @@ const commentContainer = document.querySelector(".comments");
         box.classList.add("comments__box");
         container.appendChild(box);
 
-
         const boxHead = document.createElement("div");
         boxHead.classList.add("comments__box-head");
         box.appendChild(boxHead);
-
 
         const name = document.createElement("h3");
         name.classList.add("comments__name");
         name.textContent = comments.name;
         boxHead.appendChild(name);
 
-
         const date = document.createElement("p");
         date.classList.add("comments__date");
         date.textContent = comments.date;
         boxHead.appendChild(date);
 
-
         const comment = document.createElement("p");
         comment.classList.add("comments__comment");
         comment.textContent = comments.comment;
         box.appendChild(comment);
-    
-       
+});
+
+};
+
+displayComment();
+
+
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        
+        commentContainer.innerHTML = " ";
+        const formInput = document.querySelector('.form__input');
+        const formText = document.querySelector('.form__input--textarea');
+        
+        const newComment = 
+            {
+                name: document.querySelector(".form__input").value,
+                date: Date(), // Need to figure out the date format
+                comment: document.querySelector(".form__input--textarea").value,
+            };
+        
+            console.log(newComment);
+            commentCards.unshift(newComment);
+            commentCards.pop();
+            displayComment();
+            formInput.innerHTML = "";
+            formText.innerHTML = "";
+        
     });
+    
+
 
     
 
